90bc39d0196a5470cfba8f2a84227b180444ec8d
Pippa H <pippa+github@pippah.net>
2025-08-07 09:23:01 +0200

    Remove feedbucket

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from './providers';
@@ -26,15 +25,6 @@ export default function RootLayout({
       <head>
         <link rel="dns-prefetch" href={NEXT_PUBLIC_API_URL} />
         <link rel="preconnect" href={NEXT_PUBLIC_API_URL} crossOrigin="use-credentials" />
-        <Script id="feedbucket" strategy="afterInteractive">
-          {`
-            (function(k,s) {
-                s=document.createElement('script');s.module=true;s.async=true;
-                s.src="https://cdn.feedbucket.app/assets/feedbucket.js";
-                s.dataset.feedbucket=k;document.head.appendChild(s);
-            })('3bGZapSdjMzCKHXnZ3By')
-          `}
-        </Script>
       </head>
       <body className={inter.className}>
         <Providers>
