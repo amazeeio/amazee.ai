1101ef47cb4b0ca9cd78e75b1b1217ee3756f028
Dimitris Spachos <dimitris.spachos@amazee.io>
2026-03-17 16:02:10 +0200

    Hotfix/ai fix missing db migrations 3 (#331)

--- app/migrations/versions/20260317_130000_3c1a9f7b2d11_repair_teams_columns_drift.py
+++ app/migrations/versions/20260317_130000_3c1a9f7b2d11_repair_teams_columns_drift.py
@@ -8,9 +8,8 @@ Create Date: 2026-03-17 13:00:00.000000+00:00
 
 from typing import Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 revision: str = "3c1a9f7b2d11"
 down_revision: Union[str, None] = "06fc68e254b7"
